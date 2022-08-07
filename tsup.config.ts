190ae2b6153c55ca976876b0e50498c4268e2769
Paco <34928425+pacocoursey@users.noreply.github.com>
2022-08-07 17:17:44 -0400

    Fix up repo config (#6)
    * remove external dependencies
    
    * put test site one folder higher

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,2 +1,12 @@
-import config from '../../tsup.config'
-export default config
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  sourcemap: false,
+  minify: true,
+  dts: true,
+  splitting: true,
+  format: ['esm', 'cjs'],
+  loader: {
+    '.js': 'jsx',
+  },
+})
