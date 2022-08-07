e3eeccad6b01a48880bd560437f0f6eff09016b8
rauno <freiberggg@gmail.com>
2022-08-07 12:55:12 +0300

    Reduce animation scale

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -86,9 +86,9 @@ export default function Index() {
 function CMDKWrapper(props: MotionProps & { children: React.ReactNode }) {
   return (
     <motion.div
-      initial={{ opacity: 0, scale: 0.96 }}
+      initial={{ opacity: 0, scale: 0.98 }}
       animate={{ opacity: 1, scale: 1 }}
-      exit={{ opacity: 0, scale: 0.96 }}
+      exit={{ opacity: 0, scale: 0.98 }}
       transition={{ duration: 0.2 }}
       style={{
         height: 475,
