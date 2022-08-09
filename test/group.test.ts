6638874f69dee9b7c45322a83d00ae0c29fd70fe
Paco <34928425+pacocoursey@users.noreply.github.com>
2022-08-09 16:15:27 -0400

    go back to [hidden] on group instead of changing the render structure

--- test/group.test.ts
+++ test/group.test.ts
@@ -7,15 +7,15 @@ test.describe('group', async () => {
 
   test('groups are shown/hidden based on item matches', async ({ page }) => {
     await page.locator(`[cmdk-input]`).type('z')
-    await expect(page.locator(`[cmdk-group][data-value="animals"]`)).not.toHaveCount(1)
-    await expect(page.locator(`[cmdk-group][data-value="letters"]`)).toHaveCount(1)
+    await expect(page.locator(`[cmdk-group][data-value="animals"]`)).not.toBeVisible()
+    await expect(page.locator(`[cmdk-group][data-value="letters"]`)).toBeVisible()
   })
 
   test('group can be progressively rendered', async ({ page }) => {
-    await expect(page.locator(`[cmdk-group][data-value="numbers"]`)).not.toHaveCount(1)
+    await expect(page.locator(`[cmdk-group][data-value="numbers"]`)).not.toBeVisible()
     await page.locator(`[cmdk-input]`).type('t')
-    await expect(page.locator(`[cmdk-group][data-value="animals"]`)).not.toHaveCount(1)
-    await expect(page.locator(`[cmdk-group][data-value="letters"]`)).not.toHaveCount(1)
-    await expect(page.locator(`[cmdk-group][data-value="numbers"]`)).toHaveCount(1)
+    await expect(page.locator(`[cmdk-group][data-value="animals"]`)).not.toBeVisible()
+    await expect(page.locator(`[cmdk-group][data-value="letters"]`)).not.toBeVisible()
+    await expect(page.locator(`[cmdk-group][data-value="numbers"]`)).toBeVisible()
   })
 })
