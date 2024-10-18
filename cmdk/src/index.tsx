503f4c2c267d0b71543ac601c19c24f6ab9daa6e
Sid Jain <sid_26@outlook.com>
2024-10-18 07:51:37 +0530

    rename context method

--- cmdk/src/index.tsx
+++ cmdk/src/index.tsx
@@ -122,7 +122,7 @@ type Context = {
   group: (id: string) => () => void
   filter: () => boolean
   label: string
-  shouldDisablePointerSelection: () => boolean
+  getDisablePointerSelection: () => boolean
   // Ids
   listId: string
   labelId: string
@@ -342,7 +342,7 @@ const Command = React.forwardRef<HTMLDivElement, CommandProps>((props, forwarded
         return propsRef.current.shouldFilter
       },
       label: label || props['aria-label'],
-      shouldDisablePointerSelection: () => {
+      getDisablePointerSelection: () => {
         return propsRef.current.disablePointerSelection
       },
       listId,
@@ -706,7 +706,7 @@ const Item = React.forwardRef<HTMLDivElement, ItemProps>((props, forwardedRef) =
       aria-selected={Boolean(selected)}
       data-disabled={Boolean(disabled)}
       data-selected={Boolean(selected)}
-      onPointerMove={disabled || context.shouldDisablePointerSelection() ? undefined : select}
+      onPointerMove={disabled || context.getDisablePointerSelection() ? undefined : select}
       onClick={disabled ? undefined : onSelect}
     >
       {props.children}
